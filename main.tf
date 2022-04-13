0b04d329f174cd6ee3649fbcf117ddf692a30695
Chalvin Wiradhika <chalvinwz@gmail.com>
2022-04-13 15:55:15 +0700

    Apply azure container instance

--- main.tf
+++ main.tf
@@ -32,3 +32,24 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Standard"
   admin_enabled       = true
 }
+
+resource "azurerm_container_group" "aci" {
+  name                = "pipeline-example-backend"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_address_type     = "public"
+  dns_name_label      = "aci-label"
+  os_type             = "Linux"
+
+  container {
+    name   = "pipeline-example-backend"
+    image  = "chalvinwz/pipeline-example-backend"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+}
