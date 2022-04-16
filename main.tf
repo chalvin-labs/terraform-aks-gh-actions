28baa892073edeccc0fb22c1fc64a5492de7daab
Chalvin Wiradhika <chalvinwz@gmail.com>
2022-04-16 10:50:24 +0700

    Apply AKS

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = true
 }
 
-resource "azurerm_kubernetes_cluster" "example" {
+resource "azurerm_kubernetes_cluster" "aks" {
   name                = "pipeline-example-cluster"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
