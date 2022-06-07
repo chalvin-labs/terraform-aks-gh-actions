3ed5106b917537dba89a3501952e8437d939b1b0
Chalvin Wiradhika <chalvinwz@gmail.com>
2022-06-07 08:38:58 +0700

    Remove Resources

--- main.tf
+++ main.tf
@@ -25,28 +25,28 @@ resource "azurerm_resource_group" "rg" {
   location = "eastus"
 }
 
-resource "azurerm_container_registry" "acr" {
-  name                = "pipelinexample"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "Standard"
-  admin_enabled       = true
-}
+# resource "azurerm_container_registry" "acr" {
+#   name                = "pipelinexample"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   sku                 = "Standard"
+#   admin_enabled       = true
+# }
 
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "pipeline-example-cluster"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "pipeline-example-cluster"
-  kubernetes_version  = "1.21.9"
+# resource "azurerm_kubernetes_cluster" "aks" {
+#   name                = "pipeline-example-cluster"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   dns_prefix          = "pipeline-example-cluster"
+#   kubernetes_version  = "1.21.9"
 
-  default_node_pool {
-    name       = "default"
-    node_count = 2
-    vm_size    = "Standard_B2s"
-  }
+#   default_node_pool {
+#     name       = "default"
+#     node_count = 2
+#     vm_size    = "Standard_B2s"
+#   }
 
-  identity {
-    type = "SystemAssigned"
-  }
-}
+#   identity {
+#     type = "SystemAssigned"
+#   }
+# }
